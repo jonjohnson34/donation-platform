2f2814628a256464a89c342ecddbb7e6fbd9eb44
jonjohnson34 <jr234@gmail.com>
2019-05-05 19:39:14 +0000

    added the forms for create and edit a program

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { LoginComponent } from '../app/login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DonorProfileComponent } from './donor-profile/donor-profile.component';
 import { AgencyProfileComponent } from './agency-profile/agency-profile.component';
+import { ProgramCreateComponent } from './program/program-create/program-create.component';
+import { ProgramEditComponent } from './program/program-edit/program-edit.component';
+import { ProgramListComponent } from './program/program-list/program-list.component';
 
 const routes: Routes = [
-  {path: '',       component: HomepageComponent },
-  {path: 'login',  component: LoginComponent },
-  {path: 'register', component: SignupComponent},
-  {path: 'donor', component: DonorProfileComponent},
-  {path: 'agency', component: AgencyProfileComponent}
+  {path: '',                  component: HomepageComponent },
+  {path: 'login',             component: LoginComponent },
+  {path: 'register',          component: SignupComponent},
+  {path: 'donor',             component: DonorProfileComponent},
+  {path: 'agency',            component: AgencyProfileComponent},
+  {path: 'program',           component: ProgramListComponent},
+  {path: 'program/create',    component: ProgramCreateComponent},
+  {path: 'program/edit/:id',  component: ProgramEditComponent}
 ];
 
 @NgModule({
